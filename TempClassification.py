7cfc691fb167be3d5103ed22ba46d169f253c8f0
racerandom <kevin.cheng.fei@gmail.com>
2018-05-24 21:29:36 +0900

    cnn tuning

--- TempClassification.py
+++ TempClassification.py
@@ -31,6 +31,9 @@ dev_word_in, dev_pos_in, dev_rel_in = prepare_data(doc_dic, TBD_DEV, word_idx, p
 
 test_word_in, test_pos_in, test_rel_in = prepare_data(doc_dic, TBD_TEST, word_idx, pos_idx, rel_idx, max_len, types=['Event-Event'])
 
+dev_word_in, dev_pos_in, dev_rel_in = dev_word_in.to(device=device), dev_pos_in.to(device=device), dev_rel_in.to(device=device)
+test_word_in, test_pos_in, test_rel_in = test_word_in.to(device=device), test_pos_in.to(device=device), test_rel_in.to(device=device)
+
 print(word_in.size(), pos_in.size(), rel_in.unsqueeze(1).size())
 print(word_in.dtype, pos_in.dtype, rel_in.unsqueeze(1).dtype)
 
